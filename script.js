ea7471d1a95c02cdf67638babe5a8a0040089f71
Vera Witting <verawitting@gmail.com>
2026-02-23 13:13:25 +0100

    added tests, axe core and LCP, that prints result in console. Ran in chrome and found no accessibility issues, and in firefox I got one, saying h1 needs to look different from the h2 elements.

--- script.js
+++ script.js
@@ -19,3 +19,29 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 });
+
+window.addEventListener("load", () => {
+  axe.run(document, {}, (err, results) => {
+    if (err) throw err;
+
+    console.log("Axe accessibility results:", results);
+
+    if (results.violations.length === 0) {
+      console.log("No accessibility issues found!");
+    } else {
+      console.log("Accessibility issues:", results.violations);
+    }
+  });
+});
+
+const lcpObserver = new PerformanceObserver((entryList) => {
+  const entries = entryList.getEntries();
+  const lastEntry = entries[entries.length - 1];
+
+  console.log("📊 LCP value:", lastEntry.startTime, "ms");
+});
+
+lcpObserver.observe({
+  type: "largest-contentful-paint",
+  buffered: true,
+});
